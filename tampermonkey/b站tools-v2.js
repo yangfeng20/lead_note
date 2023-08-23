434b3685c169186970fde98fdca018ac310071d1
yangfeng20 <2478341687@qq.com>
2023-08-23 15:33:36 +0800

    perf: 日志使用npm cdn

--- tampermonkey/b站tools-v2.js
+++ tampermonkey/b站tools-v2.js
@@ -6,103 +6,11 @@
 // @author       maple
 // @match        https://www.bilibili.com/*
 // @require      https://cdn.bootcss.com/jquery/3.5.0/jquery.min.js
+// @require      https://unpkg.com/maple-lib@1.0.1/log.js
 // @icon         https://www.bilibili.com/favicon.ico
 // @grant        none
 // ==/UserScript==
 "use strict";
-
-class LogLevelError extends Error {
-    constructor(msg) {
-        super(msg);
-        this.name = "LogLevelError";
-    }
-}
-
-
-let Logger = (function () {
-    class Logger {
-        constructor(level) {
-            level = level ? level : "warn"
-            this.levelArr = ["debug", "info", "warn", "error"]
-            if (!this.levelArr.includes(level)) {
-                throw new LogLevelError("日志级别错误");
-            }
-            this.level = level;
-        }
-
-        isOut(level) {
-            let curLevel = this.getLogLevel(level);
-            let sysLevel = this.getLogLevel(this.level);
-            return curLevel >= sysLevel;
-        }
-
-        getLogLevel(level) {
-            for (let i = 0; i < this.levelArr.length; i++) {
-                if (level === this.levelArr[i]) {
-                    return i;
-                }
-            }
-
-            throw new LogLevelError("错误的日志级别");
-        }
-
-        log(str, curLevel, style, structObject) {
-            if (!this.isOut(curLevel)) {
-                return;
-            }
-
-            if (structObject) {
-                console.log("%c----------------", style)
-            }
-            console.log("%c" + str, style)
-            if (structObject) {
-                console.log(structObject)
-                console.log("%c----------------", style)
-            }
-        }
-
-
-        debug(str, data) {
-            let curLevel = "debug";
-            let color = "#e39e4a";
-            let style = "color: " + color + ";font-size: 16px;";
-            this.log(str, curLevel, style, data);
-        }
-
-        info(str, data) {
-            let curLevel = "info";
-            let color = "#07b4a6";
-            let style = "color: " + color + ";font-size: 16px;";
-            this.log(str, curLevel, style, data);
-        }
-
-        warn(str, data) {
-            let curLevel = "warn";
-            let color = "#f9f400";
-            let style = "color: " + color + ";font-size: 16px;";
-            this.log(str, curLevel, style, data);
-        }
-
-        error(str, data) {
-            let curLevel = "error";
-            let color = "#f3063f";
-            let style = "color: " + color + ";font-size: 16px;";
-            this.log(str, curLevel, style, data);
-        }
-
-    }
-
-
-    let instance = null;
-    return {
-        log(level) {
-            if (!instance) {
-                instance = new Logger(level);
-            }
-            return instance;
-        }
-    }
-})();
 
 let logger = Logger.log("info")
 
